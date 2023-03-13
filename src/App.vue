838101b92f8e325773f97f6b513883c0b110bac5
MaionePietro <pietromaionespam@gmail.com>
2023-03-13 15:00:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
