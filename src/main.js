06b4be8850ec4d719570ab65d27bbbfc2a783ed9
MaionePietro <pietromaionespam@gmail.com>
2023-03-14 11:57:52 +0100

    FIX icon

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faClock, faPhone, faEnvelope, faUser} from '@fortawesome/free-solid-svg-icons'
+import { faClock, faPhone, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+
 library.add( faClock, faPhone, faEnvelope, faFacebookF, faTwitter, faUser)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
